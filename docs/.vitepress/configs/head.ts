06ee8c50c25317e856cb5721436ab54ccef3dd67
Moe-hacker <moe-hacker@outlook.com>
2025-07-18 18:22:47 +0800

    clean unused files

--- docs/.vitepress/configs/head.ts
+++
@@ -1,7 +0,0 @@
-import { HeadConfig } from "vitepress";
-
-// https://developer.mozilla.org/en-US/docs/Web/HTML
-export const head: HeadConfig[] = [
-  ["link", { rel: "icon", href: "/favicon.png" }],
-  ["meta", { name: "author", content: "Moe-hacker" }],
-];
